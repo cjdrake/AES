5bb0d229ed3e7964facf9ae0ccef71880ac22098
Chris Drake <cjdrake@gmail.com>
2014-06-18 20:37:29 -0700

    Miscellaneous code cleanup

--- hvl/aes_dpi_pkg.sv
+++ hvl/aes_dpi_pkg.sv
@@ -6,9 +6,19 @@
 package aes_dpi_pkg;
 
     import "DPI" function void
-    aes_encrypt_dpi(output bit [127:0] ct, input bit [255:0] key, int Nk, bit [127:0] pt);
+    aes_encrypt_dpi(
+        output bit [127:0] ct,
+        input bit [255:0] key,
+        int Nk,
+        bit [127:0] pt
+    );
 
     import "DPI" function void
-    aes_decrypt_dpi(output bit [127:0] pt, input bit [255:0] key, int Nk, bit [127:0] ct);
+    aes_decrypt_dpi(
+        output bit [127:0] pt,
+        input bit [255:0] key,
+        int Nk,
+        bit [127:0] ct
+    );
 
 endpackage: aes_dpi_pkg
