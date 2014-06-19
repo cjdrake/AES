12d8106bb3f9cd48e3f45371e356e232b8522943
Chris Drake <cjdrake@gmail.com>
2014-06-19 08:04:27 -0700

    Fixup DPI interface variable names

--- hvl/aes_dpi.c
+++ hvl/aes_dpi.c
@@ -9,55 +9,55 @@
 #include "aes.h"
 
 void
-aes_encrypt_dpi(int Nk, svBitVecVal * ct, svBitVecVal * pt, svBitVecVal * key)
+aes_encrypt_dpi(int Nk, svBitVecVal * bv_ct, svBitVecVal * bv_pt, svBitVecVal * bv_key)
 {
     int i;
 
-    byte_t KEY[32];
-    byte_t RKEY[240];
-    byte_t PT[16], CT[16];
+    byte_t key[32];
+    byte_t rkey[240];
+    byte_t pt[16], ct[16];
 
     byte_t * bp;
     word_t * wp;
 
     // Number of 32-bit words comprising the Cipher Key.
     // For this standard, Nk = 4, 6, or 8. (Sec 6.3)
-    bp = (byte_t *) key;
-    for (i = 0; i < 4*Nk; i++) KEY[i] = *bp++;
+    bp = (byte_t *) bv_key;
+    for (i = 0; i < 4*Nk; i++) key[i] = *bp++;
 
-    bp = (byte_t *) pt;
-    for (i = 0; i < 16; i++) PT[i] = *bp++;
+    bp = (byte_t *) bv_pt;
+    for (i = 0; i < 16; i++) pt[i] = *bp++;
 
-    KeyExpansion(Nk, RKEY, KEY);
-    Cipher(Nk, CT, PT, RKEY);
+    KeyExpansion(Nk, rkey, key);
+    Cipher(Nk, ct, pt, rkey);
 
-    wp = (word_t *) CT;
-    for (i = 0; i < 4; i++) ct[i] = *wp++;
+    wp = (word_t *) ct;
+    for (i = 0; i < 4; i++) bv_ct[i] = *wp++;
 }
 
 void
-aes_decrypt_dpi(int Nk, svBitVecVal * pt, svBitVecVal * ct, svBitVecVal * key)
+aes_decrypt_dpi(int Nk, svBitVecVal * bv_pt, svBitVecVal * bv_ct, svBitVecVal * bv_key)
 {
     int i;
 
-    byte_t KEY[32];
-    byte_t RKEY[240];
-    byte_t PT[16], CT[16];
+    byte_t key[32];
+    byte_t rkey[240];
+    byte_t pt[16], ct[16];
 
     byte_t * bp;
     word_t * wp;
 
     // Number of 32-bit words comprising the Cipher Key.
     // For this standard, Nk = 4, 6, or 8. (Sec 6.3)
-    bp = (byte_t *) key;
-    for (i = 0; i < 4*Nk; i++) KEY[i] = *bp++;
+    bp = (byte_t *) bv_key;
+    for (i = 0; i < 4*Nk; i++) key[i] = *bp++;
 
-    bp = (byte_t *) ct;
-    for (i = 0; i < 16; i++) CT[i] = *bp++;
+    bp = (byte_t *) bv_ct;
+    for (i = 0; i < 16; i++) ct[i] = *bp++;
 
-    KeyExpansion(Nk, RKEY, KEY);
-    InvCipher(Nk, PT, CT, RKEY);
+    KeyExpansion(Nk, rkey, key);
+    InvCipher(Nk, pt, ct, rkey);
 
-    wp = (word_t *) PT;
-    for (i = 0; i < 4; i++) pt[i] = *wp++;
+    wp = (word_t *) pt;
+    for (i = 0; i < 4; i++) bv_pt[i] = *wp++;
 }
